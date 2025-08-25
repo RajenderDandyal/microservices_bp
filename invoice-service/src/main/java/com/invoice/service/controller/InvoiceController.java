81ab6e4a7094c352d2118f9991a38eb512cff539
rajender dindyal <rajender@rajender.local>
2025-08-25 20:52:12 +0530

    feat: added user service folder and fixed bugs

---
+++ invoice-service/src/main/java/com/invoice/service/controller/InvoiceController.java
@@ -0,0 +1,33 @@
+package com.invoice.service.controller;
+
+
+import com.invoice.service.dto.InvoiceDTO;
+import com.invoice.service.service.InvoiceService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1")
+public class InvoiceController {
+    @Autowired
+    private InvoiceService invoiceService;
+
+    @GetMapping("/invoices")
+    public List<InvoiceDTO> getInvoices(@RequestParam("user_id") String userId, HttpServletRequest request) {
+        log.info("Received request for userId : {} ", userId);
+
+        return invoiceService.getInvoiceList(userId, request);
+    }
+
+}
--- invoice-service/src/test/java/com/invoice/service/InvoiceServiceApplicationTests.java
+++ invoice-service/src/test/java/com/invoice/service/InvoiceServiceApplicationTests.java
@@ -3,12 +3,13 @@ package com.invoice.service;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class InvoiceServiceApplicationTests {
 
 	@Test
-	@Disabled
 	void contextLoads() {
 	}
 
