633e1a6cf6f7a4403b630f15cdac8e5651ac8ce0
rajender dindyal <rajender@rajender.local>
2025-08-29 09:25:45 +0530

    feat: added openfeign retry and api-gateway

--- invoice-service/src/main/java/com/yourcompany/invoice/service/controller/InvoiceController.java
+++ invoice-service/src/main/java/com/yourcompany/invoice/service/controller/InvoiceController.java
@@ -18,7 +18,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/v1")
 public class InvoiceController {
     @Autowired
     private InvoiceService invoiceService;
